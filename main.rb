1eda3336488590919b21bd19ebe6a20f5a583996
Michael Johnsey <mjohnsey@gmail.com>
2015-12-12 16:35:06 -0600

    Wrap collection from TweetStream into a method

--- main.rb
+++ main.rb
@@ -12,7 +12,7 @@ def print_debug(debug_string)
   puts debug_string if DEBUGGING
 end
 
-very_beginning = Stopwatch.new('very_beginning')
+very_beginning = Stopwatch.new 'very_beginning'
 
 CONSUMER_KEY = config['CONSUMER_KEY']
 CONSUMER_SECRET = config['CONSUMER_SECRET']
@@ -22,36 +22,38 @@ ACCESS_TOKEN_SECRET = config['ACCESS_TOKEN_SECRET']
 STOP_AFTER_MINUTES = config['STOP_AFTER_MINUTES']
 STOP_AFTER_SECONDS = STOP_AFTER_MINUTES * 60
 
-tweets = []
+def read_via_tweetstream
+  tweets = []
 
-# start stream gather
-TweetStream.configure do |config|
-  config.consumer_key       = CONSUMER_KEY
-  config.consumer_secret    = CONSUMER_SECRET
-  config.oauth_token        = ACCESS_TOKEN
-  config.oauth_token_secret = ACCESS_TOKEN_SECRET
-  config.auth_method        = :oauth
+  # start stream gather
+  TweetStream.configure do |config|
+    config.consumer_key       = CONSUMER_KEY
+    config.consumer_secret    = CONSUMER_SECRET
+    config.oauth_token        = ACCESS_TOKEN
+    config.oauth_token_secret = ACCESS_TOKEN_SECRET
+    config.auth_method        = :oauth
+  end
+
+
+  EM.run do
+    client = TweetStream::Client.new
+
+    client.on_error do |error|
+      print_debug error
+    end
+
+    client.sample do |tweet|
+      tweets << Tweet.new(tweet.text,tweet.uris.map { |uri|  uri.expanded_url})
+      client.stop if tweets.size >= 1000 #stops after a certain amount of tweets collected
+    end
+
+    EM::Timer.new(STOP_AFTER_SECONDS) do
+      client.stop
+    end
+  end
+
+  return tweets
 end
-
-stream_catcher = Stopwatch.new('stream_catcher')
-EM.run do
-  client = TweetStream::Client.new
-
-  client.on_error do |error|
-    print_debug(error)
-  end
-
-  client.sample do |tweet|
-    tweets << Tweet.new(tweet.text,tweet.uris.map { |uri|  uri.expanded_url})
-    client.stop if tweets.size >= 1000 #stops after a certain amount of tweets collected
-  end
-
-  EM::Timer.new(STOP_AFTER_SECONDS) do
-    client.stop
-  end
-end
-
-secs_reading_the_stream = stream_catcher.elapsed_time.to_i
 
 def print_top_10_domains(tweets)
   print_debug 'Top 10 Domains:'
@@ -86,9 +88,12 @@ def print_counts(tweets,seconds_reading_the_stream)
   print_debug "Count of tweets with Instagram links: #{tweets_with_instagram_count}"
   print_debug "Percent of tweets with instagram links: #{(tweets_with_instagram_count.to_f / tweets.length.to_f * 100.0).round(2)}%"
 
-  print_top_10_domains(tweets)
+  print_top_10_domains tweets
 end
 
+stream_catcher = Stopwatch.new 'stream_catcher'
+tweets = read_via_tweetstream
+secs_reading_the_stream = stream_catcher.elapsed_time.to_i
 print_counts tweets,secs_reading_the_stream
 
 # print_debug("Total seconds elapsed: #{very_beginning.elapsed_time}")
